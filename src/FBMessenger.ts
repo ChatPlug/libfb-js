ec8cec838c9a3a74f250030f0439827077fd48e4
Bjornskjald <github@bjorn.ml>
2018-09-19 11:32:07 +0200

    Added email/password type checking

--- src/FBMessenger.ts
+++ src/FBMessenger.ts
@@ -1,6 +1,7 @@
 import FacebookApi from './FacebookApi'
 
 const login = async (email: string, password: string) => {
+  if (!email || !email.trim() || !password || !password.trim()) throw new Error('Wrong username/password!') // trim to check for spaces (which are truthy)
   const api = new FacebookApi
   await api.doLogin(email, password)
 };
