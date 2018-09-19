e6bbd14364e8f7eb3140eb4f330430d2c4e5d4ab
Bjornskjald <github@bjorn.ml>
2018-09-19 11:32:19 +0200

    Added dev config

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 import login from './src/FBMessenger';
 
-login('asd', 'asd').then().catch()
+let config
+try {
+  config = require('./config.json')
+} catch (err) {
+  console.dir(err)
+  process.exit(1)
+}
+
+login(config.email, config.password).then().catch()
