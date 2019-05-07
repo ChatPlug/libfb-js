d74cc232443a20e570c9751d884d6f983f667da3
Bjornskjald <github@bjorn.ml>
2019-05-08 01:43:24 +0200

    Fixed attachments being undefined/null

--- src/types/message/parseDeltaMessage.ts
+++ src/types/message/parseDeltaMessage.ts
@@ -4,7 +4,7 @@ import Message from '../Message'
 export default function parseDeltaMessage (delta: any) {
   return {
     threadId: getThreadId(delta),
-    attachments: delta.attachments ? delta.attachments.map(parseAttachment) : null,
+    attachments: delta.attachments ? delta.attachments.map(parseAttachment) : [],
     authorId: delta.messageMetadata.actorFbId,
     id: delta.messageMetadata.messageId,
     timestamp: delta.messageMetadata.timestamp,
