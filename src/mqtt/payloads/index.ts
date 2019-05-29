1ea0729fa0a774bb9b466be320ce4e90a4f85d68
Roughy <mdawn700@gmail.com>
2019-05-29 03:44:56 +0200

    Connect thrift message reworked with data from android app, consolidated with other thrift payloads

--- src/mqtt/payloads/index.ts
+++ src/mqtt/payloads/index.ts
@@ -1,6 +1,7 @@
 import Payload from './Payload'
 
 export { Payload }
+export { default as Connect } from './ConnectPayload'
 export { default as PresenceState } from './PresenceStatePayload'
 export { default as ReadReceipt } from './ReadReceiptPayload'
 export { default as TypingState } from './TypingStatePayload'
